ad9292f96db95352d5ab52aff96403e58dd70978
Arawn-Davies <19764552+Arawn-Davies@users.noreply.github.com>
2025-09-11 23:37:09 +0100

    Rename test vectors

--- hashtest/Benchmarks.cs
+++ hashtest/Benchmarks.cs
@@ -85,7 +85,7 @@ namespace hashtest
         /// Benchmark method to run through predefined test vectors.
         /// </summary>
         [Benchmark]
-        public void MicroHash_TestVectors()
+        public void TestVectors()
         {
             var key = new byte[16] { 0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09, 0x0A, 0x0B, 0x0C, 0x0D, 0x0E, 0x0F };
 
